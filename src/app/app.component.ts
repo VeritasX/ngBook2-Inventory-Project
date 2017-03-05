54cb5762981f8c6345162a6d304baabe57f39c52
Aaron Endsley <aaronendsley@gmail.com>
2017-03-05 17:56:30 -0600

    setting up the top level component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,30 @@ import { Product } from 'app/app.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-product: Product;
+product: Product[];
 
 constructor(){
-  this.product = new Product('NICEHAT',
+  this.product = [ new Product('NICEHAT',
   'A Nice Black Hat',
   '/resources/images/products/black-hat.jpg',
   ['Men', 'Accesories', 'hats'],
-  29.99 );
+  29.99 ),
+  new Product(
+    'NEATOJACKET',
+    'Blue Jacket',
+    '/resources/images/products/blue-jacket.jpg',
+    ['Women', 'Apparel', 'Jackets & Vests'],
+    238.99
+  ),
+  new Product('MYSHOES',
+  'Black Running Shoes',
+  '/resources/images/products/black-shoes.jpg', 
+  ['Men' , 'Shoes' , 'Running Shoes'], 
+  109.99)
+  ];
+}
+productWasSelected(product: Product): void{
+ console.log('Product clicked: ', product);
 }
 
 }
